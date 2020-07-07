33c8989730b0cc693c8519e2edf19b72ebfcf979
Eshanezer <eshan.dilmina.d@outlook.com>
2020-07-07 05:52:39 +0530

    Complete Process  ->  * Configurations with V5.2  * Routes  * Controllers

---
+++ app/Http/routes.php
@@ -0,0 +1,115 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+/*
+ * Route use karanne url path eka hadanna ... neh url eka awama kothantada yannna ona kiyala denna
+ * SPRING wala wagema thamai - user/new - new user kenek hadana page ekata yanna
+*/
+
+use const App\Http\Controllers\PostContoller;
+
+/*Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/about',function (){
+    return " Hi , About Page" ;
+});
+
+Route::get('/contact',function (){
+    return " Hi, Contact Page";
+});
+
+/*Required Parameters*/
+
+/*Route::get('/post/{id}/{name}',function ($id,$name){
+return "This is post number  ".$id." " .$name;
+});
+
+/*Optional Parameters*/
+
+/*Route::get('user/{name?}', function ($name = null) {
+    return $name;
+});*/
+
+/*Named Routes*/
+/*Route::get('admin/posts/example',array('as'=>'admin.posts',function(){
+    $url = route('admin.posts');
+
+     //<a href="route('admin.posts')">CLICK HERE</a>
+    return "This url is " .$url;
+}));
+
+
+Route::match(['get', 'post'], '/', function () {
+    //
+});
+
+Route::any('foo', function () {
+    //
+});*/
+
+
+
+/*  CONTROLLERS */
+
+/*Route::get('/post','PostContoller@index');*/
+
+
+Route::resource('posts','PostContoller');
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| MY STUDY AREA
+|--------------------------------------------------------------------------
+*/
+/*
+ * /index kiyala awath welcome page ekata yanna
+ * */
+/*Route::get('/index', function () {
+    return view('welcome');
+});*/
+
+/*
+ * /home kiyala awath Hi Eshan kiyana eka return karanna
+ * */
+/*Route::get('/home', function () {
+    return " Hi , Eshan";
+});*/
+
+/*Route::get('/admin/dashboard', function () {
+    return " Admin Area - Dashboard !";
+});*/
+
+/*
+|--------------------------------------------------------------------------
+| MY STUDY AREA - END
+|--------------------------------------------------------------------------
+*/
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
